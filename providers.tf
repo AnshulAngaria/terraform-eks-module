d8a2be587c8be8ae21a0ad0c1aa085452f4b68cb
Anshul Angaria <angaria.anshul38@gmail.com>
2022-07-23 22:09:28 +0530

    added provider

---
+++ providers.tf
@@ -0,0 +1,16 @@
+#
+# Provider Configuration
+#
+
+provider "aws" {
+  region  = "ap-south-1"
+  shared_credentials_file 	= "~/.aws/credentials"
+  profile                	= "default"
+}
+
+
+data "aws_region" "current" {}
+
+data "aws_availability_zones" "available" {}
+
+provider "http" {}
