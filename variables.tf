323c5ac6ec73c38aea70b064532a2439dd9aad58
Anshul Angaria <angaria.anshul38@gmail.com>
2022-07-23 23:08:46 +0530

    node group name

--- variables.tf
+++ variables.tf
@@ -8,3 +8,8 @@ variable "instance_types" {
   default = "t2.micro"
   type = string
 }
+
+variable "node_group_name" {
+  default = "eks-node-group"
+  type = string
+}
--- worker-nodes.tf
+++ worker-nodes.tf
@@ -40,7 +40,7 @@ resource "aws_iam_role_policy_attachment" "node_role-AmazonEC2ContainerRegistryR
 
 resource "aws_eks_node_group" "demo" {
   cluster_name    = aws_eks_cluster.demo.name
-  node_group_name = "demo"
+  node_group_name = var.node_group_name
   node_role_arn   = aws_iam_role.node_role.arn
   subnet_ids      = aws_subnet.demo[*].id
   instance_types  = [var.instance_types]
