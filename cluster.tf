738322f8f9010561bdbafd7dc82e0faf19780709
Anshul Angaria <angaria.anshul38@gmail.com>
2022-07-23 23:03:07 +0530

    created seperate file for sg

--- cluster.tf
+++ cluster.tf
@@ -28,32 +28,6 @@ resource "aws_iam_role_policy_attachment" "cluster_role-AmazonEKSServicePolicy" 
   role       = aws_iam_role.cluster_role.name
 }
 
-resource "aws_security_group" "cluster_sg" {
-  name        = "terraform-eks-cluster_sg"
-  description = "Cluster communication with worker nodes"
-  vpc_id      = aws_vpc.demo.id
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "terraform-eks-demo"
-  }
-}
-
-resource "aws_security_group_rule" "cluster_sg-ingress-workstation-https" {
-  cidr_blocks       = [aws_vpc.demo.cidr_block]
-  description       = "Allow workstation to communicate with the cluster API Server"
-  from_port         = 443
-  protocol          = "tcp"
-  security_group_id = aws_security_group.cluster_sg.id
-  to_port           = 443
-  type              = "ingress"
-}
 
 resource "aws_eks_cluster" "demo" {
   name     = var.cluster-name
---
+++ security-group.tf
@@ -0,0 +1,26 @@
+resource "aws_security_group" "cluster_sg" {
+  name        = "terraform-eks-cluster_sg"
+  description = "Cluster communication with worker nodes"
+  vpc_id      = aws_vpc.demo.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "terraform-eks-demo"
+  }
+}
+
+resource "aws_security_group_rule" "cluster_sg-ingress-workstation-https" {
+  cidr_blocks       = [aws_vpc.demo.cidr_block]
+  description       = "Allow workstation to communicate with the cluster API Server"
+  from_port         = 443
+  protocol          = "tcp"
+  security_group_id = aws_security_group.cluster_sg.id
+  to_port           = 443
+  type              = "ingress"
+}
